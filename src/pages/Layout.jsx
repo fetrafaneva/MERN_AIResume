873abd7659a02b3beff552b49504c7ee5a3e71b7
2shining <fetrafaneva@gmail.com>
2025-12-07 18:59:00 +0300

    quelque configuration et ajout du componnent Banner dans le component Home

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <div>
+      <h1>Layout Page</h1>
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
