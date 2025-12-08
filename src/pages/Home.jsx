5d4c8bae8f8c64cf188aae0bd266191928632810
2shining <fetrafaneva@gmail.com>
2025-12-08 10:26:53 +0300

    ajout de la section Feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../components/home/Banner";
 import Hero from "../components/home/Hero";
+import Feature from "../components/home/Feature";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <Hero />
+      <Feature />
     </div>
   );
 };
