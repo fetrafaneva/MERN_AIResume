0a2bbb7458a161c8dc1e305be4b551796101395b
2shining <fetrafaneva@gmail.com>
2025-12-08 18:24:20 +0300

    quelque modification du components Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,9 @@
-import { PlusIcon, UploadCloudIcon } from "lucide-react";
+import { FilePenLineIcon, PlusIcon, UploadCloudIcon } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import { dummyResumeData } from "../assets/assets";
 
 const Dashboard = () => {
+  const colors = ["#9333ea", "#d97706", "#dc2626", "#0284c7", "#16a34a"];
   const [allResumes, setAllResumes] = useState([]);
 
   const loadAllResumes = async () => {
@@ -37,6 +38,36 @@ const Dashboard = () => {
         </div>
 
         <hr className=" border-slate-300 my-6 sm:w-[305px]" />
+
+        <div className="grid grid-cols-2 sm:flex flex-wrap gap-4">
+          {allResumes.map((resume, index) => {
+            const baseColor = colors[index % colors.length];
+            return (
+              <button
+                key={index}
+                className=" relative w-full sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 border group hover:shadow-lg transition-all duration-300 cursor-pointer"
+                style={{
+                  background:
+                    "linear-gradient(135deg, ${baseColor}10, ${baseColor}40)",
+                  borderColor: baseColor + "40",
+                }}
+              >
+                <FilePenLineIcon
+                  className=" size-7 group-hover:scale-105 transition-all"
+                  style={{ color: baseColor }}
+                />
+                <p
+                  className=" text-sm group-hover:scale-105 transition-all px-2 text-center"
+                  style={{ color: baseColor }}
+                >
+                  {resume.title}
+                </p>
+
+                <p></p>
+              </button>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
