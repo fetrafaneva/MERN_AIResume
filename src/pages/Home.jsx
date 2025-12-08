e2b828a7148f259e7bf00911ffc23d0f29c05694
2shining <fetrafaneva@gmail.com>
2025-12-08 11:53:52 +0300

    ajout de la section testimonials

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "../components/home/Banner";
 import Hero from "../components/home/Hero";
 import Feature from "../components/home/Feature";
+import Testimonial from "../components/home/Testimonial";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Banner />
       <Hero />
       <Feature />
+      <Testimonial />
     </div>
   );
 };
