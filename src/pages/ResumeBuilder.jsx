8cb3403735d5bf0b539695c2bedc1e36fe6b3376
2shining <fetrafaneva@gmail.com>
2025-12-10 16:16:26 +0300

    Ajout du composant ResumeBuilder avec chargement automatique des données d'un CV via l'ID URL.

--- src/pages/ResumeBuilder.jsx
+++ src/pages/ResumeBuilder.jsx
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { dummyResumeData } from "../assets/assets";
 
 const ResumeBuilder = () => {
+  const { resumeId } = useParams();
+
+  const [resumeData, setResumeData] = useState({
+    _id: "",
+    title: "",
+    personal_info: {},
+    professional_summary: "",
+    experience: [],
+    education: [],
+    project: [],
+    skills: [],
+    template: "classic",
+    accent_color: "#3B82F6",
+    public: false,
+  });
+
+  const loadExistingResume = async () => {
+    const resume = dummyResumeData.find((resume) => resume._id === resumeId);
+    if (resume) {
+      setResumeData(resume);
+      document.title = resume.title;
+    }
+  };
+
+  useEffect(() => {
+    loadExistingResume();
+  }, []);
+
   return <div>ResumeBuilder</div>;
 };
 
