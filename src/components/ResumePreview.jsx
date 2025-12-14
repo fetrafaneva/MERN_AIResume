338f9847f1812986da79627602c37420cdd13092
2shining <fetrafaneva@gmail.com>
2025-12-14 10:01:09 +0300

    refonte du style du compenents ResumePreview

--- src/components/ResumePreview.jsx
+++ src/components/ResumePreview.jsx
@@ -27,6 +27,41 @@ const ResumePreview = ({ data, template, accentColor, classes = "" }) => {
       >
         {renderTemplate()}
       </div>
+
+      <style jsx>
+        {`
+          @page {
+            size: letter;
+            margin: 0;
+          }
+          @media print {
+            html,
+            body {
+              width: 8.5in;
+              height: 11in;
+              overflow: hidden;
+            }
+            body * {
+              visibility: hidden;
+            }
+            #resume-preview,
+            #resume-preview * {
+              visibility: visible;
+            }
+            #resume-preview {
+              position: absolute;
+              left: 0;
+              top: 0;
+              width: 100%;
+              height: auto;
+              margin: 0;
+              padding: 0;
+              box-shadow: none !important;
+              border: none !important;
+            }
+          }
+        `}
+      </style>
     </div>
   );
 };
