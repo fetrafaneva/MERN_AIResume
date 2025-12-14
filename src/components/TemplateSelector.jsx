11ae93cfae3542131c2f7158d2f3a95e3157c145
2shining <fetrafaneva@gmail.com>
2025-12-14 10:43:15 +0300

    ajout du boutton de selection de template

--- src/components/TemplateSelector.jsx
+++ src/components/TemplateSelector.jsx
@@ -1,3 +1,4 @@
+import { Layout } from "lucide-react";
 import React, { useState } from "react";
 import { preview } from "vite";
 
@@ -28,7 +29,17 @@ const TemplateSelector = ({ selectedTemplate, onChange }) => {
       preview: "Ultra-clean design that puts your content front and center",
     },
   ];
-  return <div></div>;
+  return (
+    <div className="relative">
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className=" flex items-center gap-1 text-sm text-blue-600 bg-gradient-to-br from-blue-50 to-blue-100 ring-blue-300 hover:ring transition-all px-3 py-2 rounded-lg"
+      >
+        <Layout size={14} />
+        <span className=" max-sm:hidden">Template</span>
+      </button>
+    </div>
+  );
 };
 
 export default TemplateSelector;
