4c6d4c59e34f4c31a2a7ce8935132b8d5e7880c9
2shining <fetrafaneva@gmail.com>
2025-12-16 18:28:14 +0300

    mis en place du tableau des couleurs et la mise en place du boutton

---
+++ src/components/ColorPicker.jsx
@@ -0,0 +1,30 @@
+import { Palette } from "lucide-react";
+import React, { useState } from "react";
+
+const ColorPicker = () => {
+  const colors = [
+    { name: "Blue", value: "#3B82F6" },
+    { name: "Indigo", value: "#6366F1" },
+    { name: "Purple", value: "#8B5CF6" },
+    { name: "Green", value: "#10B981" },
+    { name: "Red", value: "#EF4444" },
+    { name: "Orange", value: "#F97316" },
+    { name: "Teal", value: "#14B8A6" },
+    { name: "Pink", value: "#EC4899" },
+    { name: "Gray", value: "#6B7280" },
+    { name: "Black", value: "#1F2937" },
+  ];
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="relative">
+      <button className=" flex items-center gap-1 text-sm text-purple-600 bg-gradient-to-br from-purple-50 to-purple-100 ring-purple-300 hover:ring transition-all px-3 py-2 rounded-lg">
+        <Palette size={16} />
+        <span className="max-sm:hidden">Accent</span>
+      </button>
+    </div>
+  );
+};
+
+export default ColorPicker;
