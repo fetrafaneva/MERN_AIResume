b7b9887b55d80fd3c9739657a6b155678c6d8f04
2shining <fetrafaneva@gmail.com>
2025-12-17 19:59:51 +0300

    mise en place de la fonction removeEducation et updateEducation

--- src/components/EducationForm.jsx
+++ src/components/EducationForm.jsx
@@ -12,6 +12,19 @@ const EducationForm = ({ data, onChange }) => {
     onChange([...data, newExperience]);
   };
 
+  const removeEducation = (index) => {
+    {
+      /** index représente la position de l’élément à supprimer dans le tableau data */
+    }
+    const updated = data.filter((_, i) => i !== index);
+    onChange(updated);
+  };
+
+  const updateEducation = (index, field, value) => {
+    const updated = [...data];
+    updated[index] = { ...updated[index], [field]: value };
+    onChange(updated);
+  };
   return <div></div>;
 };
 
