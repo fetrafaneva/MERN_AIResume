8930cb0e858ef63e239d9002c768b63333bace63
2shining <fetrafaneva@gmail.com>
2025-12-17 19:58:56 +0300

    creation du component EducationForm et de la fonction addEducation

---
+++ src/components/EducationForm.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const EducationForm = ({ data, onChange }) => {
+  const addEducation = () => {
+    const newEducation = {
+      institution: "",
+      degree: "",
+      field: "",
+      graduation_date: "",
+      gpa: "",
+    };
+    onChange([...data, newExperience]);
+  };
+
+  return <div></div>;
+};
+
+export default EducationForm;
