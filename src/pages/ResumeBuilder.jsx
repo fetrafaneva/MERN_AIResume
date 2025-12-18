3f178e5f0c5833fc143bbcb99e83ab8858aab34e
2shining <fetrafaneva@gmail.com>
2025-12-18 18:03:35 +0300

    Montage du component ProjectForm dans ResumeBuilder

--- src/pages/ResumeBuilder.jsx
+++ src/pages/ResumeBuilder.jsx
@@ -20,6 +20,7 @@ import ColorPicker from "../components/ColorPicker";
 import ProfessionalSummaryForm from "../components/ProfessionalSummaryForm";
 import ExperienceForm from "../components/ExperienceForm";
 import EducationForm from "../components/EducationForm";
+import ProjectForm from "../components/ProjectForm";
 
 const ResumeBuilder = () => {
   const { resumeId } = useParams();
@@ -191,6 +192,18 @@ const ResumeBuilder = () => {
                       setResumeData((prev) => ({
                         ...prev,
                         education: data,
+                      }))
+                    }
+                  />
+                )}
+
+                {activeSection.id === "projects" && (
+                  <ProjectForm
+                    data={resumeData.project}
+                    onChange={(data) =>
+                      setResumeData((prev) => ({
+                        ...prev,
+                        project: data,
                       }))
                     }
                   />
