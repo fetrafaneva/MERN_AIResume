646de99ec80d7eee1b57b548203dfde796acc5dc
2shining <fetrafaneva@gmail.com>
2025-12-18 19:40:04 +0300

    Montage du component SkillsForm dans ResumeBuilder

--- src/pages/ResumeBuilder.jsx
+++ src/pages/ResumeBuilder.jsx
@@ -21,6 +21,7 @@ import ProfessionalSummaryForm from "../components/ProfessionalSummaryForm";
 import ExperienceForm from "../components/ExperienceForm";
 import EducationForm from "../components/EducationForm";
 import ProjectForm from "../components/ProjectForm";
+import SkillsForm from "../components/SkillsForm";
 
 const ResumeBuilder = () => {
   const { resumeId } = useParams();
@@ -204,6 +205,18 @@ const ResumeBuilder = () => {
                       setResumeData((prev) => ({
                         ...prev,
                         project: data,
+                      }))
+                    }
+                  />
+                )}
+
+                {activeSection.id === "skills" && (
+                  <SkillsForm
+                    data={resumeData.skills}
+                    onChange={(data) =>
+                      setResumeData((prev) => ({
+                        ...prev,
+                        skills: data,
                       }))
                     }
                   />
