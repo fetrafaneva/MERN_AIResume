9a571f21da9ad274f6cf1a7ea8c1e0ac2fdcbaa9
2shining <fetrafaneva@gmail.com>
2025-12-19 18:02:06 +0300

    quelque refonte et modification du components Preview

--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { dummyResumeData } from "../assets/assets";
 
 const Preview = () => {
+  const { resumeId } = useParams();
+
+  const [resumeData, setResumeData] = useState(null);
+
+  const loadResume = async () => {
+    setResumeData(
+      dummyResumeData.find((resume) => resume._id === resumeId || null)
+    );
+  };
+
+  useEffect(() => {
+    loadResume();
+  });
   return <div>Preview</div>;
 };
 
