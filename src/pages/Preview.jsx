201420b1236118e22612bd23f117d7fe67b92b5f
2shining <fetrafaneva@gmail.com>
2025-12-19 18:21:14 +0300

    ajout du state loader

--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { dummyResumeData } from "../assets/assets";
 import ResumePreview from "../components/ResumePreview";
+import Loader from "../components/Loader";
+import { ArrowLeftIcon } from "lucide-react";
 
 const Preview = () => {
   const { resumeId } = useParams();
+
+  const [isLoading, setIsLoading] = useState(true);
 
   const [resumeData, setResumeData] = useState(null);
 
@@ -12,6 +16,7 @@ const Preview = () => {
     setResumeData(
       dummyResumeData.find((resume) => resume._id === resumeId || null)
     );
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -29,7 +34,24 @@ const Preview = () => {
       </div>
     </div>
   ) : (
-    <div className=""></div>
+    <div className="">
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="flex flex-col items-center justify-center h-screen">
+          <p className=" text-center text-6xl text-slate-400 font-medium">
+            Resume not found
+          </p>
+          <a
+            href=""
+            className=" mt-6 bg-green-500 hover:bg-green-600 text-white rounded-full px-6 h-9 m-1 ring-offset-1 ring-1 ring-green-400 flex items-center transition-colors"
+          >
+            <ArrowLeftIcon className="size-4 mr-2" />
+            go to home page
+          </a>
+        </div>
+      )}
+    </div>
   );
 };
 
