8a981c32bec0d22092f04ca908198c3e96625965
2shining <fetrafaneva@gmail.com>
2025-12-21 19:47:16 +0300

    ajout du controller for getting user by id

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -72,3 +72,22 @@ export const loginUser = async (req, res) => {
     return res.status(400).json({ message: error.message });
   }
 };
+
+// controller for getting use by id
+// GET: /api/users/data
+export const getUserById = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    // check if user exists
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    // return user
+    user.password = undefined;
+    return res.status(200).json({ user });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
