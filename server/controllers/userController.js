ab3edf29e4f800ef9ef41487591116bf284009f5
2shining <fetrafaneva@gmail.com>
2025-12-21 18:00:32 +0300

    creation du token pour chaque utilisateur

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,13 @@
 import User from "../configs/models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const generateToken = (userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+  return token;
+};
 
 // controller for user registration
 // POST: /api/users/register
