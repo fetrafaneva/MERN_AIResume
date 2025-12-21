438076c5eaf7899e0c5e905fc47771868b35f026
2shining <fetrafaneva@gmail.com>
2025-12-21 20:21:14 +0300

    configuration de userRoutes.s

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {
+  getUserById,
+  loginUser,
+  registerUser,
+} from "../controllers/userController.js";
+import protect from "../middlwares/authMiddleware.js";
+
+const userRouter = express.Router();
+
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+userRouter.get("/data", protect, getUserById);
