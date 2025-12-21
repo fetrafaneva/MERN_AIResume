21d1accc40c3c72b7a6011a0b513e6c8cc8dd53b
2shining <fetrafaneva@gmail.com>
2025-12-21 20:25:01 +0300

    finalisation de l'api pour la gestion de connexions des utilisateurs

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -11,3 +11,5 @@ const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/data", protect, getUserById);
+
+export default userRouter;
