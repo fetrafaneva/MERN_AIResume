4c036461810840f2b967a96ac59631221e21610f
2shining <fetrafaneva@gmail.com>
2025-12-27 20:30:48 +0300

    UploadResume

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,4 +1,7 @@
 // controller for enhancing a resume's professional summary
+
+import Resume from "../models/Resume.js";
+
 // POST: /api/ai/enhance-pro-sum
 export const enhanceProfesssionalSummary = async (req, res) => {
   try {
@@ -57,6 +60,45 @@ export const enhanceJobDescription = async (req, res) => {
 
     const enhancedContent = response.choices[0].message.content;
     return res.status(200).json({ enhancedContent });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+// controller for uploading a resume to the database
+// POST: /api/ai/upload-resume
+export const UploadResume = async (req, res) => {
+  try {
+    const { resumeText, title } = req.body;
+    const userId = req.userId;
+
+    if (!resumeText) {
+      return res.status(400).json({ message: "Missing required field" });
+    }
+
+    const systemPrompt =
+      "You are an expert AI Agent to extract data from resume.";
+    const userPrompt = `extract data from this resume: ${resumeText}`;
+
+    const response = await ai.chat.completions.create({
+      model: process.env.OPENAI_MODEL,
+      messages: [
+        {
+          role: "system",
+          content: systemPrompt,
+        },
+        {
+          role: "user",
+          content: userPrompt,
+        },
+      ],
+      response_format: { type: "json_object" },
+    });
+
+    const extractedData = response.choices[0].message.content;
+    const parsedData = JSON.parse(extractedData);
+    const newResume = await Resume.create({ userId, title, ...parsedData });
+    res.json({ resumeId: newResume._id });
   } catch (error) {
     return res.status(400).json({ message: error.message });
   }
