f0c27a8201dac85aaaf176e4463789a412bc2dc0
2shining <fetrafaneva@gmail.com>
2025-12-28 12:24:23 +0300

    Ajout du aiRoutes

---
+++ server/routes/aiRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import protect from "../middlwares/authMiddleware.js";
+import {
+  enhanceJobDescription,
+  enhanceProfesssionalSummary,
+  UploadResume,
+} from "../controllers/aiController.js";
+
+const aiRouter = express.Router();
+
+aiRouter.post("/enhance-pro-sum", protect, enhanceProfesssionalSummary);
+aiRouter.post("/enhance-job-desc", protect, enhanceJobDescription);
+aiRouter.post("/upload-resume", protect, UploadResume);
+
+export default aiRouter;
