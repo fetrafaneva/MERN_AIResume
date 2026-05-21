65f2eb42612eb0ca173f17b5ffb22ef186e4ba22
2shining <fetrafaneva@gmail.com>
2026-05-21 20:54:58 +0300

    feat: update the home page design

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -9,7 +9,6 @@ import {
   XIcon,
 } from "lucide-react";
 import React, { useEffect, useState } from "react";
-import { dummyResumeData } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import api from "../configs/api";
