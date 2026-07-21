7f98d59d778db0f634dbda5e66c02bb88970f5ad
2shining <fetrafaneva@gmail.com>
2026-07-21 10:59:52 +0300

    feat: update footer component

--- client/src/components/home/CallToAction.jsx
+++ client/src/components/home/CallToAction.jsx
@@ -8,13 +8,14 @@ const CallToAction = () => {
     >
       <div className="flex flex-col md:flex-row text-center md:text-left items-center justify-between gap-8 px-3 md:px-10 border-x border-dashed border-slate-200 py-16 sm:py-20 -mt-10 -mb-10 w-full">
         <p className="text-xl font-medium max-w-md text-slate-800">
-          Build a Professional Resume That Helps You Stand Out and Get Hired
+          Créez un CV professionnel qui vous démarque et vous aide à décrocher
+          un emploi.{" "}
         </p>
         <a
-          href="https://prebuiltui.com"
+          href="/app"
           className="flex items-center gap-2 rounded py-3 px-8 bg-green-600 hover:bg-green-700 transition text-white"
         >
-          <span>Get Started</span>
+          <span>Commencer</span>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
