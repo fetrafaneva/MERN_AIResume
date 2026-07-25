80b0f25056ac0f66bdef82cb4f183af40f0b1b87
2shining <fetrafaneva@gmail.com>
2026-07-25 18:44:43 +0300

    feat: update userController and routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,6 +4,8 @@ import {
   getUserResumes,
   loginUser,
   registerUser,
+  verifyOtp,
+  resendOtp,
   consumeDownload,
   dismissActivationNotice,
 } from "../controllers/userController.js";
@@ -12,6 +14,8 @@ import protect from "../middlwares/authMiddleware.js";
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
+userRouter.post("/verify-otp", verifyOtp);
+userRouter.post("/resend-otp", resendOtp);
 userRouter.post("/login", loginUser);
 userRouter.get("/data", protect, getUserById);
 userRouter.get("/resumes", protect, getUserResumes);
